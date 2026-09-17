3b18d1ba062e87405f4648672e7b1808c4ae1b04
Samuelviuribe <Vianasamuel828@gmail.com>
2026-09-16 22:29:31 -0500

    All functions work, and the interpeter is working as well

---
+++ funciones.py
@@ -0,0 +1,334 @@
+#sgdb
+
+lista_tablas = []
+
+
+def agregar_columna(entrada):
+        #esperar a ejecutar sentencia esperada "CREAR COLUMNA Nombre_tabla Nombre_columna Tipo_dato"
+        #comando_columnas = input("Instruccion Agregar Columna :") #se guarda en comando columna
+
+        columna = entrada.split() #se divide en elementos de la lista 
+        comando_clave_agregar_columna = columna[0] + " "+ columna[1] # se separa el comando especifico
+
+        
+        if(comando_clave_agregar_columna == "CREAR COLUMNA"): #validando comando corecto
+            if(len(columna)== 5 ): #longitud de comando provicional o no idk
+
+                temp=False
+                for tabla in lista_tablas: #recorriendo lista de tablas
+                    if(tabla["Nombre"] == columna[2]):
+                        temp = True
+                        columna_repetida = bool(0)
+                        for nombre_columna in tabla["Columnas"]:
+                            if(nombre_columna == columna[3]):
+                                columna_repetida = bool(1)
+                        if(columna_repetida == True):
+                            print("Columna ya existe")
+                        else:
+                            tabla["Columnas"][columna[3]] = columna[4]
+                            print(tabla)
+                if(temp == False):
+                    print("Tabla no encontrada")
+            else: 
+                print("Instruccion Incorrecta")
+        else:
+            print("Comando equivocado para agregar columna")
+
+
+def Crear_tabla(entrada):
+
+    #comando_usuario = input("Instruccion de Creacion de tabla ");
+    partes = entrada.split()
+
+    #print(partes[0]+' '+partes[1]);
+    #print(comando_crear)
+    #print(partes[-1]);   
+    
+    if(len(partes) == 3):
+        comando_crear = partes[0]+' '+partes[1];
+        if (comando_crear == "CREAR TABLA"):
+            print("Tabla creada con exito!")
+
+            nombre_tabla = {"Nombre":partes[2], "Columnas":{}, "Registros":[]} 
+            """nombre_tabla = {"Nombre":partes[2],"ID_User_Paciente":int,"ID_DOC_Paciente":int,
+                            "Nombres_Paciente":str,"Apellidos_Paciente ":str,
+                            "Fecha_Nacimiento_Paciente":str,"Numero_Telefonico_Paciente":str,
+                            "Correo_Electronico_Paciente":str,"Estado_Afiliacion":str} """
+            #print(nombre_tabla)
+
+            lista_tablas.append(nombre_tabla)
+            print(lista_tablas)
+
+            #agregar_columna()
+        else:
+            print("Comando Incorrecto")
+    else:
+        print("Comando Incorrecto")
+
+
+
+
+def modificar_tablas_columnas(entrada):
+    #nothing here yet
+    # MODIFICAR TABLA Nombre_Tabla Nuevo_Nombre (4)
+    
+    comando = entrada.split();
+    
+    temp=''
+    if (len(comando) == 4):
+        if(comando[0] == "MODIFICAR"):
+            if (comando[1]== "TABLA"):
+
+                for tabla in lista_tablas:
+                    if(tabla["Nombre"] == comando[2]):
+                        temp= tabla
+        
+                if(temp != ''):
+                    print("Elemento Encontrado")        
+                    tabla["Nombre"] = comando[3] #aqui se cambia el nombre 
+                    print(lista_tablas)
+                else: 
+                    print("Elemento No Encontrado")
+
+            else:
+                print("Comando Incorrecto")
+        else:
+            print("Comando Incorrecto")
+
+    # MODIFICAR COLUMNA Nombre_Tabla Nombre_Columna Nuevo_Nombre (5)
+
+    elif (len(comando) == 5):
+
+        temp_list=''
+        temp_col=''
+
+        if(comando[0] == "MODIFICAR"):
+            if(comando[1] == "COLUMNA"):
+
+                for tabla in lista_tablas:
+                    if(tabla["Nombre"] == comando[2]):
+                        temp_list=tabla
+                
+
+                if(temp_list == ''):
+                    print('Lista no encontrada')
+                else:
+                    for columna in temp_list["Columnas"]:
+                        if(columna == comando[3]):
+                            temp_col = columna
+
+                if(temp_col == ''):
+                    print("Columna no encontrada")
+                else:
+                    temp_list["Columnas"][comando[4]] = temp_list["Columnas"].pop(temp_col)
+                    print(lista_tablas)
+
+
+        # MOFIFICAR COLUMNA Nombre_Tabla Nombre_Columna TYPE Nuevo_Nombre_Type (6)
+
+    elif (len(comando) == 6):
+         temp_list=''
+         temp_col=''
+
+         if(comando[0] == "MODIFICAR"):
+            if(comando[1] == "COLUMNA"):
+
+                for tabla in lista_tablas:
+                    if(tabla["Nombre"] == comando[2]):
+                        temp_list=tabla
+
+                if(temp_list == ''):
+                    print('Lista no encontrada')
+                else:
+                    for columna in temp_list["Columnas"]:
+                        if(columna == comando[3]):
+                            temp_col = columna
+
+                if (comando[4] == "TYPE"):
+                    if(temp_col == ''):
+                        print("Columna no encontrada")
+                    else:
+                        temp_list["Columnas"][temp_col] = comando[5]
+                        print(lista_tablas)
+                else:
+                    print("Instruccion Incorrecta")
+
+                     
+def Eliminar_Tablas_Columnas(entrada):
+    #ELIMINAR TABLA Nombre_Tabla 
+
+    #entrada = input("Instruccion Eliminar : ")
+    comando = entrada.split();
+
+    temp_list = ''
+    temp_col = ''
+
+    if(len(comando) == 3):
+        if (comando[0] == "ELIMINAR"):
+            if (comando[1] == "TABLA"):
+
+                for tabla in lista_tablas:
+                    if(tabla["Nombre"] == comando[2]):
+                        temp_list = tabla
+
+                if(temp_list == ''): #no se si en este punto temp_list es un elemento o un diccionario
+                    print("No existe "+comando[2])
+                else:
+                    lista_tablas.remove(temp_list)
+                    print(comando[2]+" Se a eliminado satisfactoriamente")
+                    print(lista_tablas)
+
+    #ELIMINAR COLUMNA Nombre_Tabla Nombre_Columna
+
+    if(len(comando) == 4):
+
+        if (comando[0] == "ELIMINAR"):
+            if (comando[1] == "COLUMNA"):
+
+                for tabla in lista_tablas:
+                    if(tabla["Nombre"] == comando[2]):
+                        temp_list = tabla
+
+                if(temp_list == ''):
+                    print(temp_list + '  Tabla no encontrada')
+                else:
+                    for columna in temp_list["Columnas"]:
+                        if(columna == comando[3]):
+                            temp_col = columna
+
+                    if(temp_col == ''):
+                        print( temp_col + " Columna no encontrada")
+                    else: 
+                        temp_list["Columnas"].pop(temp_col)
+                        print(temp_col +" Columna eliminada satisfactoriamente")
+                        print(lista_tablas)
+
+        
+    # INSERTAR EN Nombre_Tabla Nombre_Columna Registros 
+def insertar_registros(entrada):
+    #entrada = input("Instruccion Insertar : ")
+    comando = entrada.split()
+
+    temp_list = ''
+    
+
+    if(comando[0] == 'INSERTAR'):
+        if(comando[1] == 'EN'):
+
+            for tabla in lista_tablas:
+                if(tabla["Nombre"] == comando[2]):
+                    temp_list = tabla
+
+            if(temp_list != ''):
+               
+                        
+
+                registro = {}
+                temp_columna= ''
+                temp_valor= ''
+
+                for i in range(3, len(comando),2):
+                    temp_columna = comando[i]
+                    temp_valor = comando[i+1]
+
+                    if(temp_columna in temp_list["Columnas"]):
+                        registro[temp_columna] = temp_valor
+                    else:
+                        print("Columna no encontrada")
+
+                temp_list["Registros"].append(registro)
+                print(lista_tablas)
+
+            else:
+                print("Tabla no existe")
+                print(lista_tablas)
+            
+
+# CONSULTAR Nombre_Tabla
+def Consultar_Registros(entrada):
+    #entrada = input("Digite el comando consulta : ")
+    comando = entrada.split()
+    temp = ''   
+
+    if(len(comando)== 2):
+        if(comando[0] == 'CONSULTAR'):
+
+            for tabla in lista_tablas:
+                if(tabla["Nombre"] == comando[1]):
+                    temp = tabla
+
+        if(temp == ''):
+            print("No existe la tabla "+ comando[1])
+        else:
+            print(temp)
+
+    elif (len(comando) == 3):
+        temp_col = ''
+
+    #CONSULTAR REGISTROS Nombre_Tabla
+   
+        if(comando[1] == "REGISTROS"):
+            if(comando[0] == "CONSULTAR"):
+                temp = '' 
+
+                for tabla in lista_tablas:
+                    if(tabla["Nombre"] == comando[2]):
+                        temp = tabla
+
+                if(temp != ''):
+                    print(temp["Registros"])
+                else:
+                    print("Tabla no encontrada")
+
+    #CONSULTAR Nombre_tabla Nombre_Columna
+        
+        if(comando[1] != "REGISTROS"):
+            if(comando[0] == "CONSULTAR"):
+
+                for tabla in lista_tablas:
+                    if(tabla["Nombre"] == comando[1]):
+                            temp = tabla
+                         
+                if(temp != ''):
+                    for columna in temp["Columnas"]:
+                        if(columna == comando[2]):
+                            temp_col = columna
+
+                if(temp_col != ''):
+                    print(temp["Columnas"][temp_col])
+                else:
+                    print("Columna no encontrada")
+
+
+    #CONSULTAR Nombre_tabla Nombre_Columna TYPE
+    # elif (len(comando) == 4):
+    #     temp_col = ''
+    #     if(comando[1] != ""):
+    #         if(comando[0] == "CONSULTAR"):
+    #             if(comando[3] == "TYPE"):
+    #                 for tabla in lista_tablas:
+    #                     if(tabla["Nombre"] == comando[1]):
+    #                         temp = tabla
+
+    #                 for columna in temp["Columnas"]:
+    #                     if(columna == comando[2]):
+    #                         temp_col = columna
+
+    #                 if( temp_col== comando[2]):
+    #                     print(temp["Columnas"][temp_col]) 
+    
+    
+
+    else:
+        print("Comando incorrecto")
+
+
+
+
+
+
+
+
+
+        
+
--- main.py
+++ main.py
@@ -1,226 +1,35 @@
-#sgdb
-
-lista_tablas = []
-
-
-def agregar_columna():
-        #esperar a ejecutar sentencia esperada "CREAR COLUMNA Nombre_tabla Nombre_columna Tipo_dato"
-        comando_columnas = input("Siguiente Instruccion :") #se guarda en comando columna
-
-        columna = comando_columnas.split() #se divide en elementos de la lista 
-        comando_clave_agregar_columna = columna[0] + " "+ columna[1] # se separa el comando especifico
-
-        
-        if(comando_clave_agregar_columna == "CREAR COLUMNA"): #validando comando corecto
-            if(len(columna)== 5 ): #longitud de comando provicional o no idk
-
-                temp=False
-                for tabla in lista_tablas: #recorriendo lista de tablas
-                    if(tabla["Nombre"] == columna[2]):
-                        temp = True
-                        columna_repetida = bool(0)
-                        for nombre_columna in tabla["Columnas"]:
-                            if(nombre_columna == columna[3]):
-                                columna_repetida = bool(1)
-                        if(columna_repetida == True):
-                            print("Columna ya existe")
-                        else:
-                            tabla["Columnas"][columna[3]] = columna[4]
-                            print(tabla)
-                if(temp == False):
-                    print("Tabla no encontrada")
-            else: 
-                print("Instruccion Incorrecta")
-        else:
-            print("Comando equivocado para agregar columna")
-
-
-def Crear_tabla():
-
-    
-    comando_usuario = input("Digite su comando ");
-    partes = comando_usuario.split();
-
-    #print(partes[0]+' '+partes[1]);
-    #print(comando_crear)
-    #print(partes[-1]);   
-    
-    if(len(partes) == 3):
-        comando_crear = partes[0]+' '+partes[1];
-        if (comando_crear == "CREAR TABLA"):
-            print("Tabla creada con exito!")
-
-            nombre_tabla = {"Nombre":partes[2], "Columnas":{}} 
-            """nombre_tabla = {"Nombre":partes[2],"ID_User_Paciente":int,"ID_DOC_Paciente":int,
-                            "Nombres_Paciente":str,"Apellidos_Paciente ":str,
-                            "Fecha_Nacimiento_Paciente":str,"Numero_Telefonico_Paciente":str,
-                            "Correo_Electronico_Paciente":str,"Estado_Afiliacion":str} """
-            #print(nombre_tabla)
-
-            lista_tablas.append(nombre_tabla)
-            print(lista_tablas)
-
-            agregar_columna()
-        else:
-            print("Comando Incorrecto")
-    else:
-        print("Comando Incorrecto")
-
-
-
-
-def modificar_tablas_columnas():
-    #nothing here yet
-    # MODIFICAR TABLA Nombre_Tabla Nuevo_Nombre (4)
-    
-
-    entrada = input("Digite la siguiente instruccion : ")
-    comando = entrada.split();
-    
-    temp=''
-    if (len(comando) == 4):
-        if(comando[0] == "MODIFICAR"):
-            if (comando[1]== "TABLA"):
-
-                for tabla in lista_tablas:
-                    if(tabla["Nombre"] == comando[2]):
-                        temp= tabla
-        
-                if(temp != ''):
-                    print("Elemento Encontrado")        
-                    tabla["Nombre"] = comando[3] #aqui se cambia el nombre 
-                    print(temp)
-                else: 
-                    print("Elemento No Encontrado")
-
-            else:
-                print("Comando Incorrecto")
-        else:
-            print("Comando Incorrecto")
-
-    # MODIFICAR COLUMNA Nombre_Tabla Nombre_Columna Nuevo_Nombre (5)
-
-    elif (len(comando) == 5):
-
-        temp_list=''
-        temp_col=''
-
-        if(comando[0] == "MODIFICAR"):
-            if(comando[1] == "COLUMNA"):
-
-                for tabla in lista_tablas:
-                    if(tabla["Nombre"] == comando[2]):
-                        temp_list=tabla
-                
-
-                if(temp_list == ''):
-                    print('Lista no encontrada')
-                else:
-                    for columna in temp_list["Columnas"]:
-                        if(columna == comando[3]):
-                            temp_col = columna
-
-                if(temp_col == ''):
-                    print("Columna no encontrada")
-                else:
-                    temp_list["Columnas"][comando[4]] = temp_list["Columnas"].pop(temp_col)
-                    print(lista_tablas)
-
-
-        # MOFIFICAR COLUMNA Nombre_Tabla Nombre_Columna TYPE Nuevo_Nombre_Type (6)
-
-    elif (len(comando) == 6):
-         temp_list=''
-         temp_col=''
-
-         if(comando[0] == "MODIFICAR"):
-            if(comando[1] == "COLUMNA"):
-
-                for tabla in lista_tablas:
-                    if(tabla["Nombre"] == comando[2]):
-                        temp_list=tabla
-
-                if(temp_list == ''):
-                    print('Lista no encontrada')
-                else:
-                    for columna in temp_list["Columnas"]:
-                        if(columna == comando[3]):
-                            temp_col = columna
-
-                if (comando[4] == "TYPE"):
-                    if(temp_col == ''):
-                        print("Columna no encontrada")
-                    else:
-                        temp_list["Columnas"][temp_col] = comando[5]
-                        print(lista_tablas)
-                else:
-                    print("Instruccion Incorrecta")
-
-                     
-def Eliminar_Tablas_Columnas():
-    #ELIMINAR TABLA Nombre_Tabla 
-
-    entrada = input("Digite la siguiente instruccion : ")
-    comando = entrada.split();
-
-    temp_list = ''
-    temp_col = ''
-
-    if(len(comando) == 3):
-        if (comando[0] == "ELIMINAR"):
-            if (comando[1] == "TABLA"):
-
-                for tabla in lista_tablas:
-                    if(tabla["Nombre"] == comando[2]):
-                        temp_list = tabla
-
-                if(temp_list == ''): #no se si en este punto temp_list es un elemento o un diccionario
-                    print("No existe "+comando[2])
-                else:
-                    lista_tablas.remove(temp_list)
-                    print(comando[2]+" Se a eliminado satisfactoriamente")
-
-    #ELIMINAR COLUMNA Nombre_Tabla Nombre_Columna
-
-    if(len(comando) == 4):
-
-        if (comando[0] == "ELIMINAR"):
-            if (comando[1] == "COLUMNA"):
-
-                for tabla in lista_tablas:
-                    if(tabla["Nombre"] == comando[2]):
-                        temp_list = tabla
-
-                if(temp_list == ''):
-                    print(temp_list + '  Tabla no encontrada')
-                else:
-                    for columna in temp_list["Columnas"]:
-                        if(columna == comando[3]):
-                            temp_col = columna
-
-                    if(temp_col == ''):
-                        print( temp_col + " Columna no encontrada")
-                    else: 
-                        temp_list["Columnas"].pop(temp_col)
-                        print(temp_col +" Columna eliminada satisfactoriamente")
-
-        
-
-            
-
-
-
-        
-
-
+from funciones import (
+    Crear_tabla,
+    agregar_columna,
+    insertar_registros,
+    Consultar_Registros,
+    modificar_tablas_columnas,
+    Eliminar_Tablas_Columnas,
+)
                 
             
 
+Marcapasos = True 
+while Marcapasos == True:
+    entrada = input("SGDB > ")
+    validation = entrada.split()
+
+    match validation[0]:
+        case "CREAR":
+            if(validation[1] == "TABLA"):
+                Crear_tabla(entrada)
+            elif(validation[1] == "COLUMNA"):
+                agregar_columna(entrada)
+        case "INSERTAR":
+            insertar_registros(entrada)
+        case "CONSULTAR":
+            Consultar_Registros(entrada)
+        case "MODIFICAR":
+            modificar_tablas_columnas(entrada)
+        case "ELIMINAR":
+            Eliminar_Tablas_Columnas(entrada)
+        case "SALIR":
+            Marcapasos = False 
 
 
 
-#llamar a la funcion para crear una tabla 
-Crear_tabla()
-Crear_tabla()
-modificar_tablas_columnas()
-Eliminar_Tablas_Columnas()
